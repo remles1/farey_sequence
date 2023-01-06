11aea356f0cff0804f5dcbd4e7998c16980f42df
remles1 <115708921+remles1@users.noreply.github.com>
2023-01-06 16:41:13 +0100

    all done

--- farey_sequence/main.cpp
+++ farey_sequence/main.cpp
@@ -2,23 +2,24 @@
 #include <vector>
 using namespace std;
 
-vector<int> farey(double n, int iter) {
+vector<int64_t> farey(double n, int iter) {
 	bool is_positive = 1;
 	if (n < 0) {
 		is_positive = 0;
 	}
 
-	long long int before_decimal = (int)n;
+	int64_t before_decimal = (int64_t)n;
 	double after_decimal = abs(n - before_decimal);
-	
-	int a = 0; // 
-	int b = 1; // representing fraction: 0/1
+	before_decimal = abs(before_decimal);
 
-	int c = 1; // 
-	int d = 1;  // representing fraction: 1/1
+	int64_t a = 0; // 
+	int64_t b = 1; // representing fraction: 0/1
 
-	int pivot_a = a + c;
-	int pivot_b = b + d;
+	int64_t c = 1; // 
+	int64_t d = 1;  // representing fraction: 1/1
+
+	int64_t pivot_a = a + c;
+	int64_t pivot_b = b + d;
 
 	for (int i = 0; i < iter; i++) {
 		pivot_a = a + c;
@@ -38,12 +39,12 @@ vector<int> farey(double n, int iter) {
 		return {pivot_a + before_decimal * pivot_b,pivot_b};
 	}
 	else {
-		return { -1 * (pivot_a + (-1 * before_decimal * pivot_b)),pivot_b };
+		return { -1 * (pivot_a + before_decimal * pivot_b),pivot_b };
 	}
 	
 }
 
 int main() {
-	vector<int> f = farey(-138.53321312,100);
+	vector<int64_t> f = farey(-206500.153123138979,1000);
 	cout << f[0] << "/" << f[1];
 }
